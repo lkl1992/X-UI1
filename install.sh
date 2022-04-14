7e5a0ae387bfa48514a6722aa0e1fe29fe61e65f
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-04-15 01:03:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -92,7 +92,6 @@ config_after_install() {
         green "面板端口设定完成"
     else
         red "已取消,所有设置项均为默认设置,请及时修改"
-        config_port=54321
     fi
 }
 
@@ -141,7 +140,6 @@ install_x-ui() {
     systemctl daemon-reload
     systemctl enable x-ui
     systemctl start x-ui
-    IP=$(curl -sm8 ip.sb -k)
     green "x-ui v${last_version} 安装完成，面板已启动"
     echo -e ""
     echo -e "x-ui 管理脚本使用方法: "
@@ -159,11 +157,6 @@ install_x-ui() {
     echo -e "x-ui install      - 安装 x-ui 面板"
     echo -e "x-ui uninstall    - 卸载 x-ui 面板"
     echo -e "----------------------------------------------"
-    if [[ echo ${IP} | grep ':' ]]; then
-        echo -e "${green}x-ui 面板登录地址：${plain} http://[${IP}]:${config_port}"
-    else
-        echo -e "${green}x-ui 面板登录地址：${plain} http://${IP}:${config_port}"
-    fi
     rm -f install.sh
 }
 
