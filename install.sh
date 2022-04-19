c3acd62825d1d5d0ed26726e9fa58a92f779360a
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-04-19 18:21:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -75,21 +75,26 @@ ${PACKAGE_UPDATE[int]}
 
 #This function will be called when user installed x-ui out of sercurity
 config_after_install() {
-    yellow "出于安全考虑，安装完成后需要强制修改端口与账户密码"
-    read -p "请设置您的账户名:" config_account
-    read -p "请设置您的账户密码:" config_password
-    read -p "请设置面板访问端口:" config_port
-    yellow "请核对面板登录信息是否正确："
-    yellow "您的账户名将设定为:${config_account}"
-    yellow "您的账户密码将设定为:${config_password}"
-    yellow "您的面板访问端口将设定为:${config_port}"
-    read -p "确认设定完成？[y/n]": config_confirm
+    yellow "出于安全考虑，安装/更新完成后需要强制修改端口与账户密码"
+    read -p "确认是否继续?[y/n]": config_confirm
     if [[ x"${config_confirm}" == x"y" || x"${config_confirm}" == x"Y" ]]; then
-        yellow "确认设定,设定中"
-        /usr/local/x-ui/x-ui setting -username ${config_account} -password ${config_password}
-        green "账户密码设定完成"
-        /usr/local/x-ui/x-ui setting -port ${config_port}
-        green "面板端口设定完成"
+        read -p "请设置您的账户名:" config_account
+        read -p "请设置您的账户密码:" config_password
+        read -p "请设置面板访问端口:" config_port
+        yellow "请核对面板登录信息是否正确："
+        yellow "您的账户名将设定为:${config_account}"
+        yellow "您的账户密码将设定为:${config_password}"
+        yellow "您的面板访问端口将设定为:${config_port}"
+        read -p "确认设定完成？[y/n]": config_confirm
+        if [[ x"${config_confirm}" == x"y" || x"${config_confirm}" == x"Y" ]]; then
+            yellow "确认设定,设定中"
+            /usr/local/x-ui/x-ui setting -username ${config_account} -password ${config_password}
+            green "账户密码设定完成"
+            /usr/local/x-ui/x-ui setting -port ${config_port}
+            green "面板端口设定完成"
+        else
+            red "已取消,所有设置项均为默认设置,请及时修改"
+        fi
     else
         red "已取消,所有设置项均为默认设置,请及时修改"
     fi
